47f7335e4b4b4ea6678e62c3bea8b333c68c1926
Javiki <javipatinador@gmail.com>
2021-04-15 19:31:10 +0200

    Practica 4 sin errores

---
+++ Practica-4/jsf-demo/src/main/java/es/uma/informatica/sii/jsf/demo/Contacto.java
@@ -0,0 +1,32 @@
+package es.uma.informatica.sii.jsf.demo;
+
+public class Contacto {
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
